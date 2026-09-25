ea4c083bd4549b4fc1ba7de67b054f1e928a5a0e
yasen859 <artembalnov@yandex.ru>
2026-09-25 12:27:10 +0300

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_CTYPE, "RUS");
+	int num, num2;
+	puts("введите число");
+	scanf_s("%d", &num);
+	printf("Введено число %d\n", num);
+	puts("введите второе число");
+	scanf_s("%d", &num2);
+	printf("Введено число %d\n", num2);
+	printf("Сумма: %d\n", num2 + num);
+	printf("Разность: %d\n", num2 - num);
+	printf("Произведение: %d\n", num2 * num);
+	printf("Частное: %d\n", num2 / num);
+	printf("Остаток: %d\n", num2 % num);
+	//printf("Деление: %d\n", num2 * 1.0 / num);
+
+
+
+
+
+}
